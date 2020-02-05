f6d1c5e865edfb73862183c00906700653478e47
tetzng <ffffffffffffunk@gmail.com>
2020-02-05 16:51:28 +0900

    [set]enum

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,4 +2,16 @@ class Task < ApplicationRecord
   validates :name, presence: true
   validates :status, presence: true
   validates :priority, presence: true
+
+  enum status: {
+    waiting: 0,
+    doing: 1,
+    done: 2,
+  }
+
+  enum priority: {
+    high: 0,
+    middle: 1,
+    low: 2,
+  }
 end
