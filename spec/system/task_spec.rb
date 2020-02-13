5a208ab9c59cec0ee81ae7bd2b78e247d1446918
tetzng <ffffffffffffunk@gmail.com>
2020-02-13 14:55:58 +0900

    [fix]system_spec

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -3,10 +3,21 @@ require 'rails_helper'
 RSpec.describe 'Tasks', type: :system do
   let!(:task) { create(:task) }
 
-  scenario "displaying tasks list" do
-    visit tasks_path
+  scenario "displaying tasks in DESC order of 'created_at'" do
+    task2 = create(:task, name: "task2", created_at: Time.current + 1.day)
+    task3 = create(:task, name: "task3", created_at: Time.current + 2.days)
+    task4 = create(:task, name: "task4", created_at: Time.current + 3.days)
 
-    expect(page).to have_content task.name
+    visit tasks_path
+    first_task = all('h2')[0]
+    second_task = all('h2')[1]
+    third_task = all('h2')[2]
+    fourth_task = all('h2')[3]
+
+    expect(first_task).to have_content task4.name
+    expect(second_task).to have_content task3.name
+    expect(third_task).to have_content task2.name
+    expect(fourth_task).to have_content task.name
   end
 
   context "creating a new task" do
@@ -14,9 +25,9 @@ RSpec.describe 'Tasks', type: :system do
       visit new_task_path
 
       fill_in "タスク名", with: "たすくのなまえ"
-      select "doing", from: "ステータス"
-      select "middle", from: "優先度"
-      click_button '新規作成'
+      select "着手", from: "ステータス"
+      select "中", from: "優先度"
+      click_button '登録する'
 
       expect(current_path).to eq tasks_path
       expect(page).to have_content "タスクを作成しました"
@@ -26,7 +37,7 @@ RSpec.describe 'Tasks', type: :system do
     scenario "can't create" do
       visit new_task_path
 
-      click_button '新規作成'
+      click_button '登録する'
 
       expect(page).to have_content "タスクの作成に失敗しました"
     end
@@ -37,11 +48,11 @@ RSpec.describe 'Tasks', type: :system do
       visit edit_task_path(task)
 
       expect(page).to have_field "タスク名", with: task.name
-      expect(page).to have_select("ステータス", selected: task.status)
-      expect(page).to have_select("優先度", selected: task.priority)
+      expect(page).to have_select("ステータス", selected: "着手")
+      expect(page).to have_select("優先度", selected: "高")
 
       fill_in "タスク名", with: "あたらしいたすくのなまえ"
-      click_button '更新'
+      click_button '更新する'
 
       expect(current_path).to eq tasks_path
       expect(page).to have_content "タスクを更新しました"
@@ -53,7 +64,7 @@ RSpec.describe 'Tasks', type: :system do
 
       name_field = find_field("タスク名")
       name_field.native.clear
-      click_button '更新'
+      click_button '更新する'
 
       expect(page).to have_content "タスクの更新に失敗しました"
     end
